3fd9e874d0c18b0ea9570424f88d5ff0edd09131
AidaFig <aidafigueiredo8@gmail.com>
2021-11-17 19:32:46 -0300

    criando o Json com os dados internos da atividade

---
+++ src/data/database.js
@@ -0,0 +1,22 @@
+const bands = [
+    {
+        id: 1,
+        nome: "The Beatles",
+        descrição:"banda de rock britânica formada por John Lennon, Paul McCartney, George Harrison e Ringo Starr, é considerada a banda mais influente de todos os tempos.",
+        imagem: "https://studiosol-a.akamaihd.net/tb/cifra-blog/pt/wp-content/uploads/2019/03/the-beatles-a-banda-mais-influente-da-historia-4.jpg"
+    },
+    {
+        id: 2,
+        nome: "Imagine Dragons",
+        descrição:"banda de pop rock formada em Las Vegas nos Estados Unidos, consistindo do vocalista Dan Reynolds, do guitarrista Wayne Sermon, do baixista Ben McKee e do baterista Daniel Platzman.",
+        imagem: "https://media.resources.festicket.com/www/artists/ImagineDragons_New2.jpg"
+    },
+    {
+        id: 3,
+        nome: "Red Hot Chili Peppers",
+        descrição: "é uma banda de rock dos Estados Unidos formada em Los Angeles, Califórnia, considerada uma das maiores bandas da história do rock.",
+        imagem: "https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2017/10/red-hot-chili-peppers-john-frusciante-696x392.jpg"
+    }
+]
+
+export default bands
